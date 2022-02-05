33fbed83ce2f9d860933a375084d9ec033b4e552
Meenu Bapat <prajaktabapat94@gmail.com>
2022-02-05 01:02:39 +0100

    init project

---
+++ script.js
@@ -0,0 +1,38 @@
+let weather = {
+    apikey:"671190cf9646029a140872e0345d4778",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apikey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        document.querySelector(".city").innerText = "Weather in "  + name;
+        document.querySelector(".temp").innerText = temp +"°C";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity: "+ humidity +"%";
+        document.querySelector(".wind").innerText = "Wind Speed: "  + speed +"km/h";
+        document.querySelector(".weather").classList.remove("loading");
+
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value)
+    },
+};
+document.querySelector(".search button").addEventListener("click", function(){
+    weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if(event.key == "Enter"){
+        weather.search();
+    }
+});
+weather.fetchWeather("Denver");
